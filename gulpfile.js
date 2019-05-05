89be6a9ee7de40c6ec7ddc3d1ea3bf8413bb77b6
Keng Hwee <kenghwee.t@gmail.com>
2019-05-05 23:40:46 +0800

    setting up some gulp task and gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 const gulp = require("gulp");
+const watch = require("gulp-watch");
 
 gulp.task("default", () => {
     console.log("Awesome");
+});
+
+gulp.task("customTask", () => {
+    console.log("this will execute a custom task");
+});
+
+gulp.task("styles", () => {
+    console.log("Run some postcss task");
+});
+
+gulp.task("watch", () => {
+    watch("./app/index.html", () => {
+        gulp.start("customTask");
+    });
+
+    watch(".app/assets/styles/**/*.css", () => {
+        gulp.start("styles");
+    });
 });
