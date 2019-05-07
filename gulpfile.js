2438dc269d496517a81a0379166c6c8b5451cf4a
Keng Hwee <kenghwee.t@gmail.com>
2019-05-07 21:07:44 +0800

    completed css file structure part 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-const gulp = require("gulp");
-const watch = require("gulp-watch");
+const gulp = require("gulp"),
+    watch = require("gulp-watch"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssvars = require("postcss-simple-vars"),
+    nested = require("postcss-nested"),
+    cssImport = require("postcss-import");
 
 gulp.task("default", () => {
     console.log("Awesome");
@@ -10,7 +15,10 @@ gulp.task("customTask", () => {
 });
 
 gulp.task("styles", () => {
-    console.log("Run some postcss task");
+    return gulp
+        .src("./app/assets/styles/styles.css")
+        .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
+        .pipe(gulp.dest("./app/temp/styles"));
 });
 
 gulp.task("watch", () => {
@@ -18,7 +26,7 @@ gulp.task("watch", () => {
         gulp.start("customTask");
     });
 
-    watch(".app/assets/styles/**/*.css", () => {
+    watch("./app/assets/styles/**/*.css", () => {
         gulp.start("styles");
     });
 });
